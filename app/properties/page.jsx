b792e355fd38bf8bf961f4dec7fc92acf19d1dc7
DMach <mach.daniel@proton.me>
2024-05-09 00:04:05 +0200

    Add dynamic data in property cards, add Next components and react icons

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -3,12 +3,12 @@ import properties from "@/properties.json";
 
 const PropertiesPage = () => {
   return (
-    <section class="px-4 py-6">
-      <div class="container-xl m-auto px-4 py-6 lg:container">
+    <section className="px-4 py-6">
+      <div className="container-xl m-auto px-4 py-6 lg:container">
         {properties.length === 0 ? (
           <p>No properties found</p>
         ) : (
-          <div class="grid grid-cols-1 gap-6 md:grid-cols-3">
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
             {properties.map((property) => (
               <PropertyCard key={property._id} property={property} />
             ))}
--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -1,57 +1,101 @@
-const PropertyCard = () => {
+import Image from "next/image";
+import Link from "next/link";
+import {
+  FaBed,
+  FaBath,
+  FaRulerCombined,
+  FaMoneyBill,
+  FaMapMarker,
+} from "react-icons/fa";
+
+const PropertyCard = ({ property }) => {
+  const getRateDisplay = () => {
+    const { rates } = property;
+    let rate;
+
+    if (rates.monthly) {
+      rate = `${rates.monthly.toLocaleString()}/mo`;
+    } else if (rates.weekly) {
+      rate = `${rates.weekly.toLocaleString()}/wk`;
+    } else {
+      rate = `${rates.nightly.toLocaleString()}/night`;
+    }
+
+    return rate;
+  };
+
   return (
-    <div class="relative rounded-xl shadow-md">
-      <img
-        src="images/properties/a1.jpg"
+    <div className="relative rounded-xl shadow-md">
+      <Image
+        src={`/images/properties/${property.images[0]}`}
         alt=""
-        class="h-auto w-full rounded-t-xl"
+        sizes="100vw"
+        width={0}
+        height={0}
+        className="h-auto w-full rounded-t-xl"
       />
-      <div class="p-4">
-        <div class="mb-6 text-left md:text-center lg:text-left">
-          <div class="text-gray-600">Apartment</div>
-          <h3 class="text-xl font-bold">Boston Commons Retreat</h3>
+      <div className="p-4">
+        <div className="mb-6 text-left md:text-center lg:text-left">
+          <div className="text-gray-600">{property.type}</div>
+          <h3 className="text-xl font-bold">{property.name}</h3>
         </div>
-        <h3 class="absolute right-[10px] top-[10px] rounded-lg bg-white px-4 py-2 text-right font-bold text-blue-500 md:text-center lg:text-right">
-          $4,200/mo
+        <h3 className="absolute right-[10px] top-[10px] rounded-lg bg-white px-4 py-2 text-right font-bold text-blue-500 md:text-center lg:text-right">
+          ${getRateDisplay()}
         </h3>
 
-        <div class="mb-4 flex justify-center gap-4 text-gray-500">
+        <div className="mb-4 flex justify-center gap-4 text-gray-500">
           <p>
-            <i class="fa-solid fa-bed"></i> 3
-            <span class="md:hidden lg:inline">Beds</span>
+            <FaBed className="mr-2 inline"></FaBed>
+            {`${property.beds} `}
+            <span className="md:hidden lg:inline">Beds</span>
           </p>
           <p>
-            <i class="fa-solid fa-bath"></i> 2
-            <span class="md:hidden lg:inline">Baths</span>
+            <FaBath className="mr-2 inline"></FaBath>
+            {`${property.baths} `}
+            <span className="md:hidden lg:inline">Baths</span>
           </p>
           <p>
-            <i class="fa-solid fa-ruler-combined"></i>
-            1,500 <span class="md:hidden lg:inline">sqft</span>
-          </p>
-        </div>
-
-        <div class="mb-4 flex justify-center gap-4 text-sm text-green-900">
-          <p>
-            <i class="fa-solid fa-money-bill"></i> Weekly
-          </p>
-          <p>
-            <i class="fa-solid fa-money-bill"></i> Monthly
+            <FaRulerCombined className="mr-2 inline"></FaRulerCombined>
+            {`${property.square_feet} `}
+            <span className="md:hidden lg:inline">sqft</span>
           </p>
         </div>
 
-        <div class="mb-5 border border-gray-100"></div>
+        <div className="mb-4 flex justify-center gap-4 text-sm text-green-700">
+          {property.rates.nightly && (
+            <p>
+              <FaMoneyBill className="mr-2 inline"></FaMoneyBill>Nightly
+            </p>
+          )}
 
-        <div class="mb-4 flex flex-col justify-between lg:flex-row">
-          <div class="mb-4 flex gap-2 align-middle lg:mb-0">
-            <i class="fa-solid fa-location-dot text-lg text-orange-700"></i>
-            <span class="text-orange-700"> Boston MA </span>
+          {property.rates.weekly && (
+            <p>
+              <FaMoneyBill className="mr-2 inline"></FaMoneyBill>Weekly
+            </p>
+          )}
+
+          {property.rates.monthly && (
+            <p>
+              <FaMoneyBill className="mr-2 inline"></FaMoneyBill>Monthly
+            </p>
+          )}
+        </div>
+
+        <div className="mb-5 border border-gray-100"></div>
+
+        <div className="mb-4 flex flex-col justify-between lg:flex-row">
+          <div className="mb-4 flex gap-2 align-middle lg:mb-0">
+            <FaMapMarker className="mt-1 text-orange-700"></FaMapMarker>
+            <span className="text-orange-700">
+              {property.location.city} {property.location.state}
+            </span>
           </div>
-          <a
-            href="property.html"
-            class="h-[36px] rounded-lg bg-blue-500 px-4 py-2 text-center text-sm text-white hover:bg-blue-600"
+          <Link
+            href={`/properties/${property._id}`}
+            className="h-[36px] rounded-lg bg-blue-500 px-4 py-2 text-center text-sm text-white hover:bg-blue-600"
           >
             Details
-          </a>
+          </Link>
         </div>
       </div>
     </div>
