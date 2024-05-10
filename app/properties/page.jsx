757098ee5c8265ab626bba620bd52a9f3975af05
DMach <mach.daniel@proton.me>
2024-05-11 00:18:15 +0200

    Fetch data from the sever, remove properties.json

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,27 @@
 import PropertyCard from "@/components/PropertyCard";
-import properties from "@/properties.json";
 
-const PropertiesPage = () => {
+async function fetchProperties() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`,
+      { cache: "no-store" }
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data.");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const PropertiesPage = async () => {
+  const properties = await fetchProperties();
+
+  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
   return (
     <section className="px-4 py-6">
       <div className="container-xl m-auto px-4 py-6 lg:container">
