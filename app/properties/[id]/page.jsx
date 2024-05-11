58a9cb44437612e7b4f31f0cda3e67c8e2c881ee
DMach <mach.daniel@proton.me>
2024-05-11 23:17:27 +0200

    Add fetch single property api route, add single property fetch logic to id page

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,6 +1,36 @@
 "use client";
 
+import { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
+import { fetchProperty } from "@/utils/requests";
+
 const PropertyPage = () => {
+  const { id } = useParams();
+
+  const [property, setProperty] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPropertyData = async () => {
+      if (!id) {
+        return;
+      }
+
+      try {
+        const property = await fetchProperty(id);
+        setProperty(property);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (property === null) {
+      fetchPropertyData();
+    }
+  }, [id, property]);
+
   return <>PropertyPage</>;
 };
 
--- utils/requests.js
+++ utils/requests.js
@@ -19,4 +19,25 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+async function fetchProperty(id) {
+  try {
+    if (!apiDomain) {
+      return null;
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`, {
+      cache: "no-store"
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data.");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
