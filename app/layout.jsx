092c90756af2a8d3ba402e0602c1594fb9eb78cd
DMach <mach.daniel@proton.me>
2024-05-14 21:52:48 +0200

    Create AuthProvider component and import to layout in order to avoid making layout into client component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import "@/assets/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
 
 export const metadata = {
   title: "PropertyPulse | Find the Perfect Rental",
@@ -10,13 +11,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body className="flex h-screen flex-col justify-between">
-        <Navbar className="h-10" />
-        <main className="h10 mb-auto">{children}</main>
-        <Footer className="h-10" />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body className="flex h-screen flex-col justify-between">
+          <Navbar className="h-10" />
+          <main className="h10 mb-auto">{children}</main>
+          <Footer className="h-10" />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
