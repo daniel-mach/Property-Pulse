c8a5836872e942e675cc9be38b692bec8b93aa0e
DMach <mach.daniel@proton.me>
2024-05-15 23:15:04 +0200

    Add form for adding properties component with test data

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -1,5 +1,15 @@
+import PropertyAddForm from "@/components/PropertyAddForm";
+
 const PropertyAddPage = () => {
-	return <div>PropertyAddPage</div>;
+  return (
+    <section className="bg-blue-50">
+      <div className="container m-auto max-w-2xl py-24">
+        <div className="m-4 mb-4 rounded-md border bg-white px-6 py-8 shadow-md md:m-0">
+          <PropertyAddForm />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PropertyAddPage;
