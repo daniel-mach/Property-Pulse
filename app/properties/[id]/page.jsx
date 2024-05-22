136dfbeb15718fc118baadb2073629cf7c588b3f
DMach <mach.daniel@proton.me>
2024-05-22 23:22:25 +0200

    Add property images component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -6,6 +6,7 @@ import { useParams } from "next/navigation";
 import { fetchProperty } from "@/utils/requests";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import PropertyDetails from "@/components/PropertyDetails";
+import PropertyImages from "@/components/PropertyImages";
 import { FaArrowLeft } from "react-icons/fa";
 import Spinner from "@/components/Spinner";
 
@@ -148,6 +149,7 @@ const PropertyPage = () => {
               </div>
             </div>
           </section>
+          <PropertyImages images={property.images} />
         </>
       )}
     </>
---
+++ components/PropertyImages.jsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+const PropertyImages = ({ images }) => {
+  return (
+    <section className="bg-blue-50 p-4">
+      <div className="container mx-auto">
+        {images.length === 1 ? (
+          <Image
+            src={images[0]}
+            alt=""
+            className="mx-auto h-[400px] rounded-xl object-cover"
+            width={1800}
+            height={400}
+            priority={true}
+          />
+        ) : (
+          <div className="grid grid-cols-2 gap-4">
+            {images.map((image, index) => (
+              <div
+                key={index}
+                className={`${images.length === 3 && index === 2 ? "col-span-2" : "col-span-1"}`}
+              >
+                <Image
+                  src={image}
+                  alt=""
+                  className="h-[400px] w-full rounded-xl object-cover"
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  priority={true}
+                />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default PropertyImages;
