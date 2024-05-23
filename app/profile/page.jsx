d89d5582c0049ce9dd72ba7d72ebe04507feaffb
DMach <mach.daniel@proton.me>
2024-05-23 22:38:48 +0200

    Create profile page component

---
+++ app/profile/page.jsx
@@ -0,0 +1,103 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+import profileDefault from "@/assets/images/profile.png";
+
+const ProfilePage = () => {
+  const { data: session } = useSession();
+  const profileImage = session?.user?.image;
+  const profileName = session?.user?.name;
+  const profileEmail = session?.user?.email;
+
+  return (
+    <section className="bg-blue-50">
+      <div className="container m-auto py-24">
+        <div className="m-4 mb-4 rounded-md border bg-white px-6 py-8 shadow-md md:m-0">
+          <h1 className="mb-4 text-3xl font-bold">Your Profile</h1>
+          <div className="flex flex-col md:flex-row">
+            <div className="mx-20 mt-10 md:w-1/4">
+              <div className="mb-4">
+                <Image
+                  className="mx-auto h-32 w-32 rounded-full md:mx-0 md:h-48 md:w-48"
+                  src={profileImage || profileDefault}
+                  width={200}
+                  height={200}
+                  alt="User"
+                />
+              </div>
+              <h2 className="mb-4 text-2xl">
+                <span className="block font-bold">Name:</span>
+                {profileName}
+              </h2>
+              <h2 className="text-2xl">
+                <span className="block font-bold">Email:</span>
+                {profileEmail}
+              </h2>
+            </div>
+            <div className="md:w-3/4 md:pl-4">
+              <h2 className="mb-4 text-xl font-semibold">Your Listings</h2>
+              <div className="mb-10">
+                <a href="/property.html">
+                  <img
+                    className="h-32 w-full rounded-md object-cover"
+                    src="/images/properties/a1.jpg"
+                    alt="Property 1"
+                  />
+                </a>
+                <div className="mt-2">
+                  <p className="text-lg font-semibold">Property Title 1</p>
+                  <p className="text-gray-600">Address: 123 Main St</p>
+                </div>
+                <div className="mt-2">
+                  <a
+                    href="/add-property.html"
+                    className="mr-2 rounded-md bg-blue-500 px-3 py-3 text-white hover:bg-blue-600"
+                  >
+                    Edit
+                  </a>
+                  <button
+                    className="rounded-md bg-red-500 px-3 py-2 text-white hover:bg-red-600"
+                    type="button"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+              <div className="mb-10">
+                <a href="/property.html">
+                  <img
+                    className="h-32 w-full rounded-md object-cover"
+                    src="/images/properties/b1.jpg"
+                    alt="Property 2"
+                  />
+                </a>
+                <div className="mt-2">
+                  <p className="text-lg font-semibold">Property Title 2</p>
+                  <p className="text-gray-600">Address: 456 Elm St</p>
+                </div>
+                <div className="mt-2">
+                  <a
+                    href="/add-property.html"
+                    className="mr-2 rounded-md bg-blue-500 px-3 py-3 text-white hover:bg-blue-600"
+                  >
+                    Edit
+                  </a>
+                  <button
+                    className="rounded-md bg-red-500 px-3 py-2 text-white hover:bg-red-600"
+                    type="button"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProfilePage;
