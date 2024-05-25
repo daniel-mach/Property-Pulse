3c7caa2e362eb1417c878f6ceaf311e467ee5599
DMach <mach.daniel@proton.me>
2024-05-25 20:11:21 +0200

    Add removing Cloudinary images in the DELETE method

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -1,6 +1,7 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import { getSessionUser } from "@/utils/getSessionUser";
+import cloudinary from "@/config/cloudinary";
 
 export const GET = async (request, { params }) => {
   try {
@@ -43,6 +44,17 @@ export const DELETE = async (request, { params }) => {
 
     if (property.owner.toString() !== userId) {
       return new Response("Unauthorized", { status: 401 });
+    }
+
+    const publicIds = property.images.map((imageUrl) => {
+      const parts = imageUrl.split("/");
+      return parts.at(-1).split(".").at(0);
+    });
+
+    if (publicIds.length > 0) {
+      for (let publicId of publicIds) {
+        await cloudinary.uploader.destroy("propertypulse/" + publicId);
+      }
     }
 
     await property.deleteOne();
