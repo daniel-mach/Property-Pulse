2be6e8dd469a949a62b7f76e585faa5b51dac766
DMach <mach.daniel@proton.me>
2024-05-25 19:51:43 +0200

    Add DELETE method to the properties api

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -1,5 +1,6 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
 
 export const GET = async (request, { params }) => {
   try {
@@ -12,6 +13,40 @@ export const GET = async (request, { params }) => {
     }
 
     return new Response(JSON.stringify(property), {
+      status: 200
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
+
+export const DELETE = async (request, { params }) => {
+  try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID not found", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    const propertyId = params.id;
+    const property = await Property.findById(propertyId);
+
+    if (!property) {
+      return new Response("Property not found", { status: 404 });
+    }
+
+    if (property.owner.toString() !== userId) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    await property.deleteOne();
+
+    return new Response("Property deleted", {
       status: 200
     });
   } catch (error) {
