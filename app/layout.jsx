fa65faa2d64b75689a06293ada28642d7869dbdb
DMach <mach.daniel@proton.me>
2024-05-27 23:53:49 +0200

    Add shortcut icon metadata to prevent the 404 favicon error

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,17 @@
+import "@/assets/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import AuthProvider from "@/components/AuthProvider";
 import { ToastContainer } from "react-toastify";
-import "@/assets/styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
 
 export const metadata = {
   title: "PropertyPulse | Find the Perfect Rental",
   description: "Find your dream rental property",
-  keywords: "rental, property, find rentals, find properties"
+  keywords: "rental, property, find rentals, find properties",
+  icons: {
+    shortcut: "#"
+  }
 };
 
 const MainLayout = ({ children }) => {
