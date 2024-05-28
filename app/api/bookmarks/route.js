d60542ce583d15faaa2fb6358f3014fb0afc6986
DMach <mach.daniel@proton.me>
2024-05-28 21:20:06 +0200

    Add POST method to bookmark route

---
+++ app/api/bookmarks/route.js
@@ -0,0 +1,44 @@
+import connectDB from "@/config/database";
+import User from "@/models/User";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+
+export const dynamic = "force-dynamic";
+
+export const POST = async (request) => {
+  try {
+    await connectDB();
+
+    const { propertyId } = await request.json();
+    const sessionUser = await getSessionUser();
+
+    if (!session || !session.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+    const user = await User.findOne({ _id: userId });
+
+    let isBookmarked = user.bookmarks.includes(propertyId);
+    let message;
+
+    if (isBookmarked) {
+      user.bookmarks.pull(propertyId);
+      message = "Bookmark removed";
+      isBookmarked = false;
+    } else {
+      user.bookmarks.push(propertyId);
+      message = "Bookmark added";
+      isBookmarked = true;
+    }
+
+    await user.save();
+
+    return new Response(JSON.stringify({ message, isBookmarked }), {
+      status: 200
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
