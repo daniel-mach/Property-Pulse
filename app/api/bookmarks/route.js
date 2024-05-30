d636e57550e76f4a0bab8d46ee53a99f49beafe4
DMach <mach.daniel@proton.me>
2024-05-30 22:52:04 +0200

    Add GET api method for saved bookmarks

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -5,6 +5,27 @@ import { getSessionUser } from "@/utils/getSessionUser";
 
 export const dynamic = "force-dynamic";
 
+export const GET = async (request) => {
+  try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+    const user = await User.findOne({ _id: userId });
+    const bookmarks = await Property.find({ _id: { $in: user.bookmarks } });
+
+    return new Response(JSON.stringify(bookmarks), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
+
 export const POST = async (request) => {
   try {
     await connectDB();
