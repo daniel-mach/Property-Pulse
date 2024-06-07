5c7b8add8b469673310d5327162579101de75af9
DMach <mach.daniel@proton.me>
2024-06-07 23:00:07 +0200

    Add GET method to messages

--- app/api/messages/route.js
+++ app/api/messages/route.js
@@ -4,6 +4,31 @@ import { getSessionUser } from "@/utils/getSessionUser";
 
 export const dynamic = "force-dynamic";
 
+export const GET = async (request) => {
+  try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.user) {
+      return new Response("User ID is required", {
+        status: 401
+      });
+    }
+
+    const { userId } = sessionUser;
+
+    const messages = await Message.find({ recipient: userId })
+      .populate("sender", "name")
+      .populate("property", "title");
+
+    return new Response(JSON.stringify(messages), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
+
 export const POST = async (request) => {
   try {
     await connectDB();
