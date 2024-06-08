63427f3461b002747ff502297ceee08f18e8b731
DMach <mach.daniel@proton.me>
2024-06-08 23:31:05 +0200

    Fetch and map over user messages to display via new Message component

--- components/Messages.jsx
+++ components/Messages.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import Message from "@/components/Message";
 import Spinner from "@/components/Spinner";
 
 const Messages = () => {
@@ -26,7 +27,26 @@ const Messages = () => {
     getMessages();
   }, []);
 
-  return <div>Messages</div>;
+  return loading ? (
+    <Spinner loading={loading} />
+  ) : (
+    <section className="bg-blue-50">
+      <div className="container m-auto max-w-6xl py-24">
+        <div className="m-4 mb-4 rounded-md border bg-white px-6 py-8 shadow-md md:m-0">
+          <h1 className="mb-4 text-3xl font-bold">Your Messages</h1>
+          <div className="space-y-4">
+            {messages.length === 0 ? (
+              <p>You have no messages.</p>
+            ) : (
+              messages.map((message) => (
+                <Message key={message._id} message={message} />
+              ))
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Messages;
