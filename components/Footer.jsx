c2c11d00dbb5a21a7b482d9c4e4e402ae4f6ba7c
DMach <mach.daniel@proton.me>
2024-06-10 22:42:41 +0200

    Add gray logo variant

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from "@/assets/images/logo.png";
+import logo from "@/assets/images/logo-gray.png";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
