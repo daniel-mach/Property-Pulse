4e406938e3efcfd28a11624fffef21f307f4a23a
DMach <mach.daniel@proton.me>
2024-06-12 23:12:50 +0200

    Add DELETE method to message API calls

--- app/api/messages/[id]/route.js
+++ app/api/messages/[id]/route.js
@@ -38,3 +38,36 @@ export const PUT = async (request, { params }) => {
     return new Response("Something went wrong", { status: 500 });
   }
 };
+
+export const DELETE = async (request, { params }) => {
+  try {
+    await connectDB();
+
+    const { id } = params;
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.user) {
+      return new Response("User ID is required", {
+        status: 401
+      });
+    }
+
+    const { userId } = sessionUser;
+
+    const message = await Message.findById(id);
+
+    if (!message) return new Response("Message not found", { status: 404 });
+
+    if (message.recipient.toString() !== userId) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    await message.deleteOne();
+
+    return new Response("Message deleted", { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
