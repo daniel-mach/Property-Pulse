49759a75699f058dbfb17f6730a74c8e51352d7b
DMach <mach.daniel@proton.me>
2024-06-26 22:34:52 +0200

    Refactor Featured Properties component

--- components/PropertiesFeatured.jsx
+++ components/PropertiesFeatured.jsx
@@ -1,28 +1,32 @@
-import { fetchProperties } from "@/utils/requests";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
 import PropertyCardFeatured from "./PropertyCardFeatured";
 
 const FeaturedProperties = async () => {
-  const properties = await fetchProperties({ showFeatured: true });
+  await connectDB();
+
+  const properties = await Property.find({
+    is_featured: true
+  }).lean();
+
   const randomProperties = properties
     .sort(() => Math.random() - 0.5)
     .slice(0, 2);
 
-  return (
-    properties.length > 0 && (
-      <section className="bg-green-50 px-4 pb-12 pt-8">
-        <div className="container-xl m-auto lg:container">
-          <h2 className="mb-6 text-center text-3xl font-bold text-green-600">
-            Featured Properties
-          </h2>
-          <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-            {randomProperties.map((property) => (
-              <PropertyCardFeatured key={property._id} property={property} />
-            ))}
-          </div>
+  return properties.length > 0 ? (
+    <section className="bg-blue-50 px-4 pb-10 pt-6">
+      <div className="container-xl m-auto lg:container">
+        <h2 className="mb-6 text-center text-3xl font-bold text-green-600">
+          Featured Properties
+        </h2>
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+          {randomProperties.map((property) => (
+            <PropertyCardFeatured key={property._id} property={property} />
+          ))}
         </div>
-      </section>
-    )
-  );
+      </div>
+    </section>
+  ) : null;
 };
 
 export default FeaturedProperties;
