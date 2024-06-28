69329451465f299c6065cd6f8ecde65a7de4f909
DMach <mach.daniel@proton.me>
2024-06-28 23:12:53 +0200

    Refactor single Property page into a server component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,46 +1,27 @@
-"use client";
-
 import Link from "next/link";
-import { useState, useEffect } from "react";
-import { useParams } from "next/navigation";
-import { fetchProperty } from "@/utils/requests";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import PropertyDetails from "@/components/PropertyDetails";
 import PropertyImages from "@/components/PropertyImages";
-import BookmarkButton from "@/components/ButtonBookmark";
-import ShareButton from "@/components/ButtonShare";
-import { FaArrowAltCircleLeft } from "react-icons/fa";
-import Spinner from "@/components/Spinner";
+import BookmarkButton from "@/components/BookmarkButton";
 import PropertyContactForm from "@/components/PropertyContactForm";
+import ShareButton from "@/components/ShareButton";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { convertToSerializeableObject } from "@/utils/convertToObject";
 
-const PropertyPage = () => {
-  const { id } = useParams();
+const PropertyPage = async ({ params }) => {
+  const PUBLIC_DOMAIN = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:3000";
 
-  const [property, setProperty] = useState(null);
-  const [loading, setLoading] = useState(true);
+  await connectDB();
 
-  useEffect(() => {
-    const fetchPropertyData = async () => {
-      if (!id) {
-        return;
-      }
+  const propertyDoc = await Property.findById(params.id).lean();
 
-      try {
-        const property = await fetchProperty(id);
-        setProperty(property);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  const property = convertToSerializeableObject(propertyDoc);
 
-    if (property === null) {
-      fetchPropertyData();
-    }
-  }, [id, property]);
-
-  if (!property && !loading) {
+  if (!property) {
     return (
       <h1 className="mt-10 text-center text-2xl font-bold">
         Property Not Found
@@ -50,38 +31,32 @@ const PropertyPage = () => {
 
   return (
     <>
-      {loading && <Spinner loading={loading} />}
-      {!loading && property && (
-        <>
-          <PropertyHeaderImage image={property.images[0]} />
-          <section className="border bg-gray-50">
-            <div className="container m-auto px-6 py-6">
-              <Link
-                href="/properties"
-                className="flex items-center text-green-500 hover:text-green-600"
-              >
-                <FaArrowAltCircleLeft className="mr-2" />
-                <p>Back to Properties</p>
-              </Link>
-            </div>
-          </section>
-          <section>
-            <div className="container m-auto px-6 py-10">
-              <div className="grid w-full grid-cols-1 gap-6 md:grid-cols-70/30">
-                <PropertyDetails property={property} />
-                <aside className="space-y-4">
-                  <BookmarkButton property={property} />
-                  <ShareButton property={property} />
-                  <PropertyContactForm property={property} />
-                </aside>
-              </div>
-            </div>
-          </section>
-          <PropertyImages images={property.images} />
-        </>
-      )}
+      <PropertyHeaderImage image={property.images[0]} />
+      <section className="border bg-gray-50">
+        <div className="container m-auto px-6 py-6">
+          <Link
+            href="/properties"
+            className="flex items-center text-green-500 hover:text-green-600"
+          >
+            <FaArrowAltCircleLeft className="mr-2" />
+            <p>Back to Properties</p>
+          </Link>
+        </div>
+      </section>
+      <section>
+        <div className="container m-auto px-6 py-10">
+          <div className="grid w-full grid-cols-1 gap-6 md:grid-cols-70/30">
+            <PropertyDetails property={property} />
+            <aside className="space-y-4">
+              <BookmarkButton property={property} />
+              <ShareButton property={property} />
+              <PropertyContactForm property={property} />
+            </aside>
+          </div>
+        </div>
+      </section>
+      <PropertyImages images={property.images} />
     </>
   );
 };
-
 export default PropertyPage;
