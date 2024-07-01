dc762381d6ca6ccb67c7e0bc614a0e877b2b7485
DMach <mach.daniel@proton.me>
2024-07-01 23:02:54 +0200

    Fix profile properties import

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import profileDefault from "@/assets/images/profile.png";
-import ProfileProperties from "@/components/ProfileProperties";
+import ProfileProperties from "@/components/PropertiesProfile";
 import connectDB from "@/config/database";
 import { getSessionUser } from "@/utils/getSessionUser";
 import { convertToSerializeableObject } from "@/utils/convertToObject";
@@ -20,43 +20,41 @@ const ProfilePage = async () => {
   const properties = propertiesDocs.map(convertToSerializeableObject);
 
   return (
-    <section className="bg-blue-50">
-      <div className="container m-auto py-24">
-        <div className="m-4 mb-4 rounded-md border bg-white px-6 py-8 shadow-md md:m-0">
-          <h1 className="mb-4 text-3xl font-bold">Your Profile</h1>
-          <div className="flex flex-col md:flex-row">
-            <div className="mx-20 mt-10 md:w-1/4">
-              <div className="mb-4">
-                <Image
-                  className="mx-auto h-32 w-32 rounded-full md:mx-0 md:h-48 md:w-48"
-                  src={sessionUser.user.image || profileDefault}
-                  width={200}
-                  height={200}
-                  alt="User"
-                />
-              </div>
-              <h2 className="mb-4 text-2xl">
-                <span className="block font-bold">Name: </span>{" "}
-                {sessionUser.user.name}
-              </h2>
-              <h2 className="text-2xl">
-                <span className="block font-bold">Email: </span>{" "}
-                {sessionUser.user.email}
-              </h2>
+    <div className="container m-auto py-24">
+      <div className="m-4 mb-4 rounded-md border bg-white px-6 py-8 shadow-md md:m-0">
+        <h1 className="mb-4 text-3xl font-bold">Your Profile</h1>
+        <div className="flex flex-col md:flex-row">
+          <div className="mx-20 mt-10 md:w-1/4">
+            <div className="mb-4">
+              <Image
+                className="mx-auto h-32 w-32 rounded-full md:mx-0 md:h-48 md:w-48"
+                src={sessionUser.user.image || profileDefault}
+                width={200}
+                height={200}
+                alt="User"
+              />
             </div>
+            <h2 className="mb-4 text-2xl">
+              <span className="block font-bold">Name: </span>{" "}
+              {sessionUser.user.name}
+            </h2>
+            <h2 className="text-2xl">
+              <span className="block font-bold">Email: </span>{" "}
+              {sessionUser.user.email}
+            </h2>
+          </div>
 
-            <div className="md:w-3/4 md:pl-4">
-              <h2 className="mb-4 text-xl font-semibold">Your Listings</h2>
-              {properties.length === 0 ? (
-                <p>You have no property listings</p>
-              ) : (
-                <ProfileProperties properties={properties} />
-              )}
-            </div>
+          <div className="md:w-3/4 md:pl-4">
+            <h2 className="mb-4 text-xl font-semibold">Your Listings</h2>
+            {properties.length === 0 ? (
+              <p>You have no property listings</p>
+            ) : (
+              <ProfileProperties properties={properties} />
+            )}
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
