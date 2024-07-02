a8f7afa566e416aad4931b494f7c0be0df89ae8b
DMach <mach.daniel@proton.me>
2024-07-02 23:12:29 +0200

    Refactor search results page from client to server component

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -1,76 +1,64 @@
-"use client";
-
-import { useState, useEffect } from "react";
-import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import { FaArrowAltCircleLeft } from "react-icons/fa";
 import PropertyCard from "@/components/PropertyCard";
-import Spinner from "@/components/Spinner";
 import PropertySearchForm from "@/components/PropertySearchForm";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { convertToSerializeableObject } from "@/utils/convertToObject";
 
-const SearchResultsPage = () => {
-  const searchParams = useSearchParams();
+const SearchResultsPage = async ({
+  searchParams: { location, propertyType }
+}) => {
+  await connectDB();
 
-  const [properties, setProperties] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const locationPattern = new RegExp(location, "i");
 
-  const location = searchParams.get("location");
-  const propertyType = searchParams.get("propertyType");
+  let query = {
+    $or: [
+      { name: locationPattern },
+      { description: locationPattern },
+      { "location.street": locationPattern },
+      { "location.city": locationPattern },
+      { "location.state": locationPattern },
+      { "location.zipcode": locationPattern }
+    ]
+  };
 
-  useEffect(() => {
-    const fetchSearchResults = async () => {
-      try {
-        const response = await fetch(
-          `/api/properties/search?location=${location}&propertyType=${propertyType}`
-        );
+  if (propertyType && propertyType !== "All") {
+    const typePattern = new RegExp(propertyType, "i");
+    query.type = typePattern;
+  }
 
-        if (response.status === 200) {
-          const data = await response.json();
-          setProperties(data);
-        } else {
-          setProperties([]);
-        }
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchSearchResults();
-  }, [location, propertyType]);
+  const propertiesQueryResults = await Property.find(query).lean();
+  const properties = convertToSerializeableObject(propertiesQueryResults);
 
   return (
     <>
-      <section className="bg-green-700">
+      <section className="bg-green-700 py-4">
         <div className="mx-auto flex max-w-7xl flex-col items-center px-4 sm:px-6 lg:px-8">
           <PropertySearchForm />
         </div>
       </section>
-      {loading ? (
-        <Spinner loading={loading} />
-      ) : (
-        <section className="py-6">
-          <div className="container-xl m-auto px-4 py-6 lg:container">
-            <Link
-              href="/properties"
-              className="mb-3 flex items-center text-green-500 hover:underline"
-            >
-              <FaArrowAltCircleLeft className="mr-2" />
-              <p>Back to Properties</p>
-            </Link>
-            <h1 className="mb-4 text-2xl">Search Results</h1>
-            {properties.length === 0 ? (
-              <p>No search results found</p>
-            ) : (
-              <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
-                {properties.map((property) => (
-                  <PropertyCard key={property._id} property={property} />
-                ))}
-              </div>
-            )}
-          </div>
-        </section>
-      )}
+      <section className="py-6">
+        <div className="container-xl m-auto px-4 py-6 lg:container">
+          <Link
+            href="/properties"
+            className="mb-3 flex items-center text-green-500 hover:underline"
+          >
+            <FaArrowAltCircleLeft className="mr-2" /> Back To Properties
+          </Link>
+          <h1 className="mb-4 text-2xl">Search Results</h1>
+          {properties.length === 0 ? (
+            <p>No search results found</p>
+          ) : (
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+              {properties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
     </>
   );
 };
